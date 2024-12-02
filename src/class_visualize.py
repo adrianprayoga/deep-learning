a3b7ab94d8d227c5f5c8ed0dcf51842b96d11124
Min Ho Kim <mkim819@gatech.edu>
2024-12-03 01:51:06 +0900

    class visualize: weight loading corrected
    Signed-off-by: Min Ho Kim <mkim819@gatech.edu>

--- src/class_visualize.py
+++ src/class_visualize.py
@@ -16,13 +16,21 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 model = XceptionDetector(num_classes=2)
 state_dict = torch.load(config["weights_path"], map_location=torch.device('cpu'))
-model.load_state_dict(state_dict, strict=False)
-class_names = ["Real", "Fake"]
+if any(key.startswith("module.") for key in state_dict.keys()):
+    state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+state_dict['backbone.fc.weight'] = state_dict.pop('backbone.last_linear.weight')
+state_dict['backbone.fc.bias'] = state_dict.pop('backbone.last_linear.bias')
+missing_keys, unexpected_keys = model.load_state_dict(state_dict, strict=False)
+if missing_keys:
+    print(f"Missing keys: {missing_keys}")
+if unexpected_keys:
+    print(f"Unexpected keys: {unexpected_keys}")
 
 # Initialize ClassVisualization
 vis = ClassVisualization()
 
 # Visualize "Fake" class
+class_names = ["Real", "Fake"]
 fake_image = vis.create_class_visualization(
     target_y=1,
     class_names=class_names,
@@ -30,7 +38,7 @@ fake_image = vis.create_class_visualization(
     dtype=torch.float32,
     l2_reg=1e-3,
     learning_rate=25,
-    num_iterations=1000,
+    num_iterations=100,
     blur_every=10,
     max_jitter=16,
     show_every=25,
