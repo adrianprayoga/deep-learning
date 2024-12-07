f3fc7e2e8c4fb9b9132e063eb6e34509f8601e23
Min Ho Kim <mkim819@gatech.edu>
2024-12-07 22:39:07 +0900

    update with adriana model loading code
    Signed-off-by: Min Ho Kim <mkim819@gatech.edu>

--- src/class_visualize.py
+++ src/class_visualize.py
@@ -3,8 +3,9 @@ from pathlib import Path
 import torch
 import yaml
 import os
-from utils.model_loading import XceptionDetector
+from utils.config_utils import load_model_config
 from utils.class_visualization import ClassVisualization
+from utils.model_loading import get_detector
 import matplotlib.pyplot as plt
 
 PROJECT_DIR = Path(__file__).resolve().parent.parent
@@ -14,18 +15,10 @@ with open(os.path.join(PROJECT_DIR, 'src/config.yaml'), 'r') as f:
 # Initialize model and class names
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-model = XceptionDetector(num_classes=2)
-state_dict = torch.load(config["weights_path"], map_location=torch.device('cpu'))
-if any(key.startswith("module.") for key in state_dict.keys()):
-    state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
-state_dict['backbone.fc.weight'] = state_dict.pop('backbone.last_linear.weight')
-state_dict['backbone.fc.bias'] = state_dict.pop('backbone.last_linear.bias')
-missing_keys, unexpected_keys = model.load_state_dict(state_dict, strict=False)
-if missing_keys:
-    print(f"Missing keys: {missing_keys}")
-if unexpected_keys:
-    print(f"Unexpected keys: {unexpected_keys}")
-
+model = get_detector(config['model_type'],
+                       config=load_model_config(config['model_type']),
+                       load_weights=True,
+                       weights_path=config["weights_path"])
 # Initialize ClassVisualization
 vis = ClassVisualization()
 
@@ -38,7 +31,7 @@ fake_image = vis.create_class_visualization(
     dtype=torch.float32,
     l2_reg=1e-3,
     learning_rate=25,
-    num_iterations=100,
+    num_iterations=1000,
     blur_every=10,
     max_jitter=16,
     show_every=25,
